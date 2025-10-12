c26ad30763adb3da56b23f6d92ec3092b387fd29
Argon4W <1516775492@qq.com>
2025-10-12 16:47:03 +0800

    Fix compatibility with connector, potentially fix hotbar rendering glitches

--- src/main/java/com/github/argon4w/acceleratedrendering/compat/iris/mixins/plugin/IrisCompatMixinPlugin.java
+++ src/main/java/com/github/argon4w/acceleratedrendering/compat/iris/mixins/plugin/IrisCompatMixinPlugin.java
@@ -2,10 +2,12 @@ package com.github.argon4w.acceleratedrendering.compat.iris.mixins.plugin;
 
 import com.github.argon4w.acceleratedrendering.compat.AbstractCompatMixinPlugin;
 
+import java.util.List;
+
 public class IrisCompatMixinPlugin extends AbstractCompatMixinPlugin {
 
 	@Override
-	protected String getModID() {
-		return "iris";
+	protected List<String> getModIDs() {
+		return List.of("iris");
 	}
 }
